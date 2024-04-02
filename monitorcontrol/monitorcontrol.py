9e2c272c91328a6741017f8abdfeec62b8b97f89
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-04-02 15:40:39 -0400

    Remove CLI, long doc comments, and tests

--- monitorcontrol/monitorcontrol.py
+++ monitorcontrol/monitorcontrol.py
@@ -1,6 +1,5 @@
 from typing import List, Union
 import enum
-import sys
 
 from .vcp.vcp_abc import VCP
 from .vcp.vcp_codes import get_vcp_com
@@ -8,8 +7,6 @@ from .vcp.vcp_codes import get_vcp_com
 
 @enum.unique
 class ColorPreset(enum.Enum):
-    """Monitor color presets."""
-
     COLOR_TEMP_4000K = 0x03
     COLOR_TEMP_5000K = 0x04
     COLOR_TEMP_6500K = 0x05
@@ -25,24 +22,15 @@ class ColorPreset(enum.Enum):
 
 @enum.unique
 class PowerMode(enum.Enum):
-    """Monitor power modes."""
-
-    #: On.
     on = 0x01
-    #: Standby.
     standby = 0x02
-    #: Suspend.
     suspend = 0x03
-    #: Software power off.
     off_soft = 0x04
-    #: Hardware power off.
     off_hard = 0x05
 
 
 @enum.unique
 class InputSource(enum.Enum):
-    """Monitor input sources."""
-
     OFF = 0x00
     ANALOG1 = 0x01
     ANALOG2 = 0x02
@@ -65,124 +53,27 @@ class InputSource(enum.Enum):
 
 
 class InputSourceValueError(ValueError):
-    """
-    Raised upon an invalid (out of spec) input source value.
-
-    https://github.com/newAM/monitorcontrol/issues/93
-
-    Attributes:
-        value (int): The value of the input source that was invalid.
-    """
-
     def __init__(self, message: str, value: int):
         super().__init__(message)
         self.value = value
 
 
 def get_luminance(vcp: VCP) -> int:
-    """
-    Gets the monitors back-light luminance.
-
-    Args:
-        vcp: The VCP to act on
-
-    Returns:
-        Current luminance value.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.get_luminance())
-
-    Raises:
-        VCPError: Failed to get luminance from the VCP.
-    """
     code = get_vcp_com("image_luminance")
     return vcp.get_vcp_feature(code)[0]
 
 
 def set_luminance(vcp: VCP, value: int):
-    """
-    Sets the monitors back-light luminance.
-
-    Args:
-        vcp: The VCP to act on
-        value: New luminance value (typically 0-100).
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    monitor.set_luminance(50)
-
-    Raises:
-        ValueError: Luminance outside of valid range.
-        VCPError: Failed to set luminance in the VCP.
-    """
     code = get_vcp_com("image_luminance")
     vcp.set_vcp_feature(code, value)
 
 
 def get_color_preset(vcp: VCP) -> int:
-    """
-    Gets the monitors color preset.
-
-    Args:
-        vcp: The VCP to act on
-
-    Returns:
-        Current color preset.
-        Valid values are enumerated in :py:class:`ColorPreset`.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.get_color_preset())
-
-    Raises:
-        VCPError: Failed to get color preset from the VCP.
-    """
     code = get_vcp_com("image_color_preset")
     return vcp.get_vcp_feature(code)[0]
 
 
 def set_color_preset(vcp: VCP, value: Union[int, str, ColorPreset]):
-    """
-    Sets the monitors color preset.
-
-    Args:
-        vcp: The VCP to act on
-        value:
-            An integer color preset,
-            or a string representing the color preset,
-            or a value from :py:class:`ColorPreset`.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors, ColorPreset
-
-            for monitor in get_monitors():
-                with monitor:
-                    monitor.set_color_preset(ColorPreset.COLOR_TEMP_5000K)
-
-    Raises:
-        VCPError: Failed to set color preset in the VCP.
-        ValueError: Color preset outside valid range.
-        AttributeError: Color preset string is invalid.
-        TypeError: Unsupported value
-    """
     if isinstance(value, str):
         mode_value = getattr(ColorPreset, value).value
     elif isinstance(value, int):
@@ -197,109 +88,21 @@ def set_color_preset(vcp: VCP, value: Union[int, str, ColorPreset]):
 
 
 def get_contrast(vcp: VCP) -> int:
-    """
-    Gets the monitors contrast.
-
-    Args:
-        vcp: The VCP to act on
-
-    Returns:
-        Current contrast value.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.get_contrast())
-
-    Raises:
-        VCPError: Failed to get contrast from the VCP.
-    """
     code = get_vcp_com("image_contrast")
     return vcp.get_vcp_feature(code)[0]
 
 
 def set_contrast(vcp: VCP, value: int):
-    """
-    Sets the monitors back-light contrast.
-
-    Args:
-        vcp: The VCP to act on
-        value: New contrast value (typically 0-100).
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.set_contrast(50))
-
-    Raises:
-        ValueError: Contrast outside of valid range.
-        VCPError: Failed to set contrast in the VCP.
-    """
     code = get_vcp_com("image_contrast")
     vcp.set_vcp_feature(code, value)
 
 
 def get_power_mode(vcp: VCP) -> PowerMode:
-    """
-    Get the monitor power mode.
-
-    Args:
-        vcp: The VCP to act on
-
-    Returns:
-        Value from the :py:class:`PowerMode` enumeration.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.get_power_mode())
-
-    Raises:
-        VCPError: Failed to get the power mode.
-        ValueError: Set power state outside of valid range.
-        KeyError: Set power mode string is invalid.
-    """
     code = get_vcp_com("display_power_mode")
     return PowerMode(vcp.get_vcp_feature(code)[0])
 
 
 def set_power_mode(vcp: VCP, value: Union[int, str, PowerMode]):
-    """
-    Set the monitor power mode.
-
-    Args:
-        vcp: The VCP to act on
-        value:
-            An integer power mode,
-            or a string representing the power mode,
-            or a value from :py:class:`PowerMode`.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    monitor.set_power_mode("standby")
-
-    Raises:
-        VCPError: Failed to get or set the power mode
-        ValueError: Power state outside of valid range.
-        AttributeError: Power mode string is invalid.
-    """
     if isinstance(value, str):
         mode_value = getattr(PowerMode, value).value
     elif isinstance(value, int):
@@ -314,40 +117,6 @@ def set_power_mode(vcp: VCP, value: Union[int, str, PowerMode]):
 
 
 def get_input_source(vcp: VCP) -> InputSource:
-    """
-    Gets the monitors input source
-
-    Args:
-        vcp: The VCP to act on
-
-    Returns:
-        Current input source.
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.get_input_source())
-
-        Handling out-of-spec inputs (observed for USB type-C inputs)::
-
-            from monitorcontrol import get_monitors, InputSourceValueError
-
-            for monitor in get_monitors():
-                with monitor:
-                    try:
-                        print(monitor.get_input_source())
-                    except InputSourceValueError as e:
-                        print(e.value)
-
-    Raises:
-        VCPError: Failed to get input source from the VCP.
-        InputSourceValueError:
-            Input source value is not within the MCCS defined inputs.
-    """
     code = get_vcp_com("input_select")
     value = vcp.get_vcp_feature(code)[0] & 0xFF
     try:
@@ -357,27 +126,6 @@ def get_input_source(vcp: VCP) -> InputSource:
 
 
 def set_input_source(vcp: VCP, value: Union[int, str, InputSource]):
-    """
-    Sets the monitors input source.
-
-    Args:
-        vcp: The VCP to act on
-        value: New input source
-
-    Example:
-        Basic Usage::
-
-            from monitorcontrol import get_monitors
-
-            for monitor in get_monitors():
-                with monitor:
-                    print(monitor.set_input_source("DP1"))
-
-    Raises:
-        VCPError: Failed to get the input source.
-        KeyError: Set input source string is invalid.
-    """
-
     if isinstance(value, str):
         mode_value = getattr(InputSource, value.upper()).value
     elif isinstance(value, int):
@@ -396,44 +144,4 @@ def get_capabilities(vcp: VCP) -> dict:
 
 
 def get_vcps() -> List[VCP]:
-    """
-    Discovers virtual control vcps.
-
-    This function should not be used directly in most cases, use
-    :py:func:`get_monitors` get monitors with VCPs.
-
-    Returns:
-        List of VCPs in a closed state.
-
-    Raises:
-        NotImplementedError: not implemented for your operating system
-        VCPError: failed to list VCPs
-    """
     return VCP.get_vcps()
-
-
-# def get_monitors() -> List[Monitor]:
-#     """
-#     Creates a list of all monitors.
-#
-#     Returns:
-#         List of monitors in a closed state.
-#
-#     Raises:
-#         VCPError: Failed to list VCPs.
-#
-#     Example:
-#         Setting the power mode of all monitors to standby::
-#
-#             for monitor in get_monitors():
-#                 with monitor:
-#                     monitor.set_power_mode("standby")
-#
-#         Setting all monitors to the maximum brightness using the
-#         context manager::
-#
-#             for monitor in get_monitors():
-#                 with monitor:
-#                     monitor.set_luminance(100)
-#     """
-#     return [Monitor(v) for v in vcp.get_vcps()]
